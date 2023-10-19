33c3fc767ad98eb92b37fd28d87678feece6ed16
an_owl <an_owl@protonmail.com>
2023-10-19 13:25:50 +1300

    Changed name to bitmap-fontgen

--- test/build-single/build.rs
+++ test/build-single/build.rs
@@ -5,7 +5,7 @@ fn main() {
     let p = PathBuf::from_str("../../res/ter-u32n.bdf").unwrap().canonicalize().unwrap();
     let mut o = std::env::var("OUT_DIR").unwrap().to_string();
     o += "test_single_file.rs";
-    fontgen::codegen::gen_font(vec![p.clone()],&mut std::fs::File::create(&o).unwrap());
+    bitmap_fontgen::codegen::gen_font(vec![p.clone()],&mut std::fs::File::create(&o).unwrap());
     println!("cargo:rustc-env=FONT_FILE={}",o);
     println!("cargo:rustc-env=FONT_ORIGIN={}",p.display());
 }
